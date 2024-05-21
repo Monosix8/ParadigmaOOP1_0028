245755017f66b6af64af48f9c9219d44dfb8d976
Monosix8 <ridhohidayatullah008@gmail.com>
2024-05-21 23:02:40 +0700

    int main

--- inisialisasi.cpp
+++ inisialisasi.cpp
@@ -7,3 +7,18 @@ private:
     const int id;
     string nama;
     float nilai;
+
+public:
+    mahasiswa(int pid, string pnama, float pnilai) : id(pid), nama(pnama), nilai(pnilai) {
+
+    }
+    ~mahasiswa() {
+        cout << "id    = " << id << endl;
+        cout << "Nama  = " << nama << endl;
+        cout << "Nilai = " << nilai << endl;
+    }
+};
+
+int main() {
+    mahasiswa mhs(12, "Asroni", 90.5);
+}
